d619518b84bb5ca5492a064bacddc82d6c92ffde
Mykhailo Odrekhivskyi <mykhailo_odrekhivsky@epualviw1823.kyiv.epam.com>
2019-10-10 17:35:47 +0300

    added api for charts

--- proxy-server/src/services/socket.service.ts
+++ proxy-server/src/services/socket.service.ts
@@ -1,8 +1,11 @@
 import io from 'socket.io';
 import { Server } from 'http';
-import { Store } from './cache.service';
+import { Store } from './store.service';
 
-const store = new Store('https://api.nomics.com/v1/currencies/ticker?key=b297be749ce2b84de83d68336e851ad5&interval=1h,1d,7d,30d');
+const store = new Store(
+    'https://api.nomics.com/v1/currencies/ticker?key=b297be749ce2b84de83d68336e851ad5&interval=1h,1d,7d,30d',
+    'https://api.nomics.com/v1/currencies/sparkline?key=b297be749ce2b84de83d68336e851ad5&start=2019-10-08T10%3A59%3A59.999Z&end=2019-10-09T10%3A59%3A59.999Z'
+);
 
 const socket = (server: Server) => {
     const socketServer = io(server);
@@ -10,17 +13,23 @@ const socket = (server: Server) => {
     socketServer.on('connection', (socket) => {
         let itemsNumber = 20;
 
-        socket.emit('cryptoupdate', store.get(itemsNumber));
+        socket.emit('cryptoupdate', store.getCoinsInfo(itemsNumber));
 
         store.coinsUpdated$.subscribe((status) => {
             if (status) {
-                socket.emit('cryptoupdate', store.get(itemsNumber));
+                socket.emit('cryptoupdate', store.getCoinsInfo(itemsNumber));
+            }
+        });
+
+        store.chartsUpdated$.subscribe((status) => {
+            if (status) {
+                socket.emit('chartsupdate', store.getChartsInfo(itemsNumber));
             }
         });
 
         socket.on('getmore', () => {
             itemsNumber += 10;
-            socket.emit('cryptoupdate', store.get(itemsNumber));
+            socket.emit('cryptoupdate', store.getCoinsInfo(itemsNumber));
         })
     });
 
---
+++ proxy-server/src/services/store.service.ts
@@ -0,0 +1,57 @@
+import { Observable, BehaviorSubject, from, interval } from 'rxjs';
+import { map, take, switchMap, first } from 'rxjs/operators';
+import axios from 'axios';
+import { TInfo } from '../models/models';
+
+export class Store {
+    private coinsInfo: Array<any> = [];
+    private chartsInfo: Array<any> = [];
+    public coinsUpdated$ = new BehaviorSubject<boolean>(false);
+    public chartsUpdated$ = new BehaviorSubject<boolean>(false);
+
+    constructor(private url: string, private url2: string) {
+        this.getInfo$(this.url).pipe(
+            switchMap(
+                (coinsInfoRes) => {
+                  this.initData(coinsInfoRes, 'coinsInfo');
+                  return this.getInfo$(this.url2);
+                }
+            ),
+            first()
+        )
+        .subscribe((chartsInfoRes) => {
+            this.initData(chartsInfoRes, 'chartsInfo');
+            //this.runAutoUpdate();
+        });
+    }
+
+    private runAutoUpdate(): void {
+        interval(15000).pipe(switchMap(() => this.getInfo$(this.url)))
+        .subscribe((coinsInfoRes) => {
+            this.initData(coinsInfoRes, 'coinsInfo');
+            this.coinsUpdated$.next(true);
+        });
+
+        interval(15000).pipe(switchMap(() => this.getInfo$(this.url2)))
+        .subscribe((chartsInfoRes) => {
+            this.initData(chartsInfoRes, 'chartsInfo');
+            this.chartsUpdated$.next(true);
+        });
+    }
+
+    private getInfo$(url: string): Observable<any> {
+        return from(axios.get(url)).pipe(first(), map(res => res.data));
+    }
+
+    private initData(result: any[], resultName: TInfo): void {
+        this[resultName] = result;
+    }
+
+    public getCoinsInfo(number: number, ): string {
+        return JSON.stringify(this.coinsInfo.slice(0, number));
+    }
+
+    public getChartsInfo(number: number, ): string {
+        return JSON.stringify(this.coinsInfo.slice(0, number));
+    }
+}
